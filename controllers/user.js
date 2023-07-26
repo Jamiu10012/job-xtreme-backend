1b7c95de2d24fa1b9889c2a056f8d9842c6b9738
jamiu10012 <ibrahimjamiu026@gmail.com>
2023-07-27 00:36:50 +0100

    add user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,31 @@
-export const update = (req, res, next) => {};
-// const User = require("../models/User");
-// export const createUser = async (req, res, next) => {
-//   try {
-//     const newUser = await User.create(req.body);
-//     res.status(201).json(newUser);
-//   } catch (err) {
-//     res.status(500).json({ error: "Failed to create user." });
-//   }
-// };
-// // Get user by ID
-// export const getUserById = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id);
-//     res.status(200).json(user);
-//   } catch (err) {
-//     res.status(404).json({ error: "User not found." });
-//   }
-// };
+// export const update = (req, res, next) => {};
+import User from "../models/User.js";
+export const createUser = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to create user." });
+  }
+};
+// Get user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ error: "User not found." });
+  }
+};
 
-// // Update user by ID
-// export const updateUserById = async (req, res) => {
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//     });
-//     res.status(200).json(updatedUser);
-//   } catch (err) {
-//     res.status(404).json({ error: "User not found." });
-//   }
-// };
+// Update user by ID
+export const updateUserById = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(404).json({ error: "User not found." });
+  }
+};
