2e2f8d55459daf581c3c367810da25eaefadb4c8
jamiu10012 <ibrahimjamiu026@gmail.com>
2023-07-30 16:56:17 +0100

    on

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,27 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import { creatError } from "../error.js";
+import { createError } from "../error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+
+    // Check if the email already exists in the database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: "Email already exists." });
+    }
+
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    const hash = bcrypt.hashSync(password, salt);
+
+    // Create a new user
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
-    res.status(200).send("User has been created!");
+    res.status(201).json({ message: "User has been created!", user: newUser });
   } catch (err) {
     next(err);
   }
@@ -20,11 +30,11 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   try {
     const user = await User.findOne({ name: req.body.name });
-    if (!user) return next(creatError(404, "User not found"));
+    if (!user) return next(createError(404, "User not found"));
 
     const isCorrect = await bcrypt.compare(req.body.password, user.password);
 
-    if (!isCorrect) return next(creatError(400, "wrong credientials"));
+    if (!isCorrect) return next(createError(400, "wrong credientials"));
 
     const token = jwt.sign({ id: user._id }, process.env.JWT);
     const { password, ...others } = user._doc;
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 // export const update = (req, res, next) => {};
 import User from "../models/User.js";
+import { createError } from "../error.js";
+
 export const createUser = async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
@@ -14,18 +16,28 @@ export const getUserById = async (req, res) => {
     const user = await User.findById(req.params.id);
     res.status(200).json(user);
   } catch (err) {
-    res.status(404).json({ error: "User not found." });
+    next(err);
   }
 };
 
 // Update user by ID
-export const updateUserById = async (req, res) => {
+export const updateUserById = async (req, res, next) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
     res.status(200).json(updatedUser);
   } catch (err) {
-    res.status(404).json({ error: "User not found." });
+    next(createError(403, "You can update only your account!"));
+  }
+};
+export const deleteUserById = async (req, res, next) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json("user has been deleted");
+  } catch (err) {
+    next(createError(403, "You can delete only your account!"));
   }
 };
