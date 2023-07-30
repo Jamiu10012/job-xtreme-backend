96f6d8e6e5a700b331e8fc4c19c5ef5fc3d48e0b
jamiu10012 <ibrahimjamiu026@gmail.com>
2023-07-30 11:48:33 +0100

    on

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Import the cors middleware
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
-const connect = () => {
-  mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
 
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors()); // Add cors middleware to allow all origins. You can specify specific origins if needed.
+
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Error handling middleware
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong!";
@@ -32,7 +29,18 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(8000, () => {
-  connect();
-  console.log("Connected to server successfully");
-});
+// Connect to the database and start the server
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to DB");
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Error connecting to DB:", err);
+  }
+};
+
+startServer();
