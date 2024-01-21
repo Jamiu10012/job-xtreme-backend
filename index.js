4a1940a04af0cb7a29ecdfbdb2bdb7eba6994136
jamiu10012 <ibrahimjamiu026@gmail.com>
2024-01-21 14:02:52 +0100

    done

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ import resumeRoutes from "./routes/resume.js";
 import employerRoute from "./routes/employer.js";
 import jobApplicationRoute from "./routes/jobApplication.js";
 import savedJobRoute from "./routes/savedJob.js";
+import riskScoreRoute from "./routes/riskScoreRouter.js";
 
 const app = express();
 dotenv.config();
@@ -32,6 +33,7 @@ app.use("/api/resume", resumeRoutes);
 app.use("/api/employer", employerRoute);
 app.use("/api/jobapp", jobApplicationRoute);
 app.use("/api/savedjob", savedJobRoute);
+app.use("/api/riskScores", riskScoreRoute);
 
 // app.get("/", (req, res) => {
 //   res.send("<h1>Welcome to job Xtreme</h1>");
